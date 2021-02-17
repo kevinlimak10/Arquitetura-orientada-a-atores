52fea56390c45b400bb68aae80a9d991417f47d8
Kevin Lima <kevin.lima@cwi.com.br>
2021-02-16 21:02:45 -0300

    adicionando inicio do processo de Cluster

--- src/main/java/StartClusters.java
+++ src/main/java/StartClusters.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import atores.PokedexActor;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import mensagens.Pokemon;
 
 public class StartClusters {
+
+    private static final boolean DEVE_USAR_CLUSTER_LOCAL = true;
+
+        public static void main(String[] args) throws InterruptedException {
+            if (args.length == 0) {
+                startPokedex(args);
+            } else {
+                startup(args);
+            }
+        }
+
+    public static void startPokedex(String[] ports) throws InterruptedException {
+        ActorSystem system = ActorSystem.create("ClusterSystem", ConfigFactory.load("statusUm"));
+        ActorRef pokedex = system.actorOf(Props.create(PokedexActor.class, DEVE_USAR_CLUSTER_LOCAL), "pokedex");
+        Pokemon pokeBulba = new Pokemon(10,"Bulbasauro","mato",null);
+        Pokemon pokePiggeoto = new Pokemon(20,"Piggeoto","vento",null);
+        pokedex.tell(pokePiggeoto,ActorRef.noSender());
+        pokedex.tell(pokeBulba,ActorRef.noSender());
+    }
+
+
+    public static void startup(String[] ports) {
+        for (String port : ports) {
+            Config config = ConfigFactory
+                    .parseString(
+                            "akka.remote.netty.tcp.port=" + port + "\n" + "akka.remote.artery.canonical.port=" + port)
+                    .withFallback(ConfigFactory.parseString("akka.cluster.roles = [compute]"))
+                    .withFallback(ConfigFactory.load("statusUm"));
+
+            ActorSystem.create("ClusterSystem", config);
+        }
+    }
 }
---
+++ src/main/java/atores/AvistarPokemonActor.java
@@ -0,0 +1,54 @@
+package atores;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.cluster.routing.ClusterRouterPool;
+import akka.cluster.routing.ClusterRouterPoolSettings;
+import akka.routing.RoundRobinPool;
+import mensagens.Pokemon;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AvistarPokemonActor extends AbstractActor {
+
+    private ActorRef pokedex;
+
+
+    public AvistarPokemonActor(boolean cluster) {
+        this.pokedex = cluster ? criandoClusterRoteador() : criandoBuildLocal();
+    }
+
+
+    private ActorRef criandoBuildLocal() {
+        return getContext().actorOf(new RoundRobinPool(2).props(Props.create(PokedexActor.class)), "pokedexActor");
+    }
+
+    private ActorRef criandoClusterRoteador() {
+        int totalInstances = 2;
+        int maxInstancesPerNode = 1;
+        boolean allowLocalRoutees = false;
+        Set<String> useRoles = new HashSet<>(Arrays.asList("compute"));
+        return getContext().actorOf(new ClusterRouterPool(new RoundRobinPool(2),
+                        new ClusterRouterPoolSettings(totalInstances, maxInstancesPerNode, allowLocalRoutees, useRoles))
+                        .props(Props.create(PokemonActor.class)),
+                "pokedexRouter");
+
+    }
+
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder().
+                match(Pokemon.class, pokemon ->
+                        chamarPokedex(pokemon)).
+                build();
+    }
+
+    private void chamarPokedex(Pokemon pokemon) {
+        pokedex.tell(pokemon,getSelf());
+    }
+
+}
